974e4053c01bbb9bc2857cc68251047a64b8c9cf
cs0041 <pamzalove20@hotmail.com>
2023-01-20 01:17:36 +0700

    Update Plans.tsx

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -1,10 +1,19 @@
+import { CheckIcon } from '@heroicons/react/solid'
+import { Product } from '@stripe/firestore-stripe-payments'
 import Head from 'next/head'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import useAuth from '../hooks/useAuth'
+import Table from './Table'
 
-interface Props {}
+interface Props {
+  products: Product[] 
+}
 
-function Plans({}: Props) {
+function Plans({ products }: Props) {
+  const { logout } = useAuth()
+  const [selectedPlan,setSelectedPlan] = useState<Product>(products[2])
+
   return (
     <div>
       <Head>
@@ -12,18 +21,69 @@ function Plans({}: Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className='border-b border-white/10 bg-[#141414]'>
+      <header className="border-b border-white/10 bg-[#141414]">
         <Link href="/">
-            <img
+          <img
             src="https://rb.gy/ulxxee"
             alt="Netflix"
             width={150}
             height={90}
             className="cursor-pointer object-contain"
-            />
+          />
         </Link>
-        <button className='text-lg font-medium hover:underline'>Sign Out</button>
+        <button
+          onClick={logout}
+          className="text-lg font-medium hover:underline"
+        >
+          Sign Out
+        </button>
       </header>
+
+      <main
+        className="max-w-5xl pt-28 pb-12 transition-all
+      md:px-10"
+      >
+        <h1 className="mb-3 text-3xl font-medium">
+          Choose the plan that's right for you
+        </h1>
+        <ul>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Watch all you want.
+            Ad-free.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Recommendations
+            just for you.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Change or cancel
+            your plan anytime.
+          </li>
+        </ul>
+
+        <div className="mt-4 flex flex-col space-y-4">
+          <div
+            className="flex w-full items-center justify-end self-end md:w-3/5 " >
+              {products.map((product)=>(
+                <div
+                className={`planBox cursor-pointer ${selectedPlan.id === product.id ? "opacity-100" : "opacity-60"} `}
+                 key={product.id}
+                 onClick={()=> setSelectedPlan(product)}
+                 >
+                      {product.name}
+                </div>
+              ))}
+         
+          </div>
+          
+          <Table 
+            products={products}
+            selectedPlan={selectedPlan}
+          />
+
+          <button>Subscribe</button>
+        </div>
+      </main>
     </div>
   )
 }
