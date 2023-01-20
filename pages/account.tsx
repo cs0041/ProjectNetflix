031f41293bc28b4c6bc942f22cba0b00a85706cc
cs0041 <pamzalove20@hotmail.com>
2023-01-20 18:17:01 +0700

    Create account.tsx

---
+++ pages/account.tsx
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import React from 'react'
+import useAuth from '../hooks/useAuth'
+import useSubscription from '../hooks/useSubscription'
+
+type Props = {}
+
+function Account({}: Props) {
+    const { user } = useAuth()
+    const subscription = useSubscription(user)
+  return (
+    <div>
+      <Head>
+        <title>Account - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className={`bg-[#141414]`}>
+        <Link href="/">
+          <img
+            src="https://rb.gy/ulxxee"
+            width={120}
+            height={120}
+            className="cursor-pointer object-contain"
+          />
+        </Link>
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </header>
+
+      <main className="pt-24">
+        <div>
+          <h1 className="text-3xl md:text-4xl">Account</h1>
+          <div className='-ml-0.5 flex items-center gap-x-1.5'>
+            <img src="https://rb.gy/4vfk4r" alt="" className="h-7 w-7" />
+            <p className='text-xs font-semibold text-[#555]'>Member since {subscription?.created}</p>
+          </div>
+        </div>
+
+        {/* <Membership/> */}
+
+
+        <div>
+            <h4>Plan Details</h4>
+            <div></div>
+        </div>
+
+
+      </main>
+    </div>
+  )
+}
+
+export default Account
